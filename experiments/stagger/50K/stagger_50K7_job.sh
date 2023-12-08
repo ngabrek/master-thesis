a5595a40fe792a1f30a9602bfdddada298732d60
ngabrek <nadinegabrek@web.de>
2023-12-08 22:41:24 +0100

    Experiments STAGGER 50K

---
+++ experiments/stagger/50K/stagger_50K7_job.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+################# Begin Slurm header ####################
+#
+# Give job a reasonable name
+#SBATCH --job-name=stagger_50K7
+#
+# Request number of nodes for job
+#SBATCH --ntasks=1
+#
+# Maximum run time of job (hh:mm:ss)
+#SBATCH --time=72:00:00
+#
+#SBATCH -p single
+#
+#SBATCH --mail-type=ALL
+#
+#SBATCH --mail-user=nadinegabrek@web.de
+#
+################ End Slurm header ######################
+
+cd ..
+cd ..
+cd ..
+
+python -m experiments.stagger.50K.stagger_50K7
